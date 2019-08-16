43159876b528885d58629aff870b9251bc015c1c
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2019-08-16 12:05:25 +0200

    SEDOL check

---
+++ R/sedol.R
@@ -0,0 +1,38 @@
+sedol_compute_checksum <- function(s) {
+  u8.l <- figi_char_to_utf8(s)
+  figi_code.l <- base::lapply(u8.l, FUN = figi_utf8_to_code)
+  mulw.l <- base::lapply(figi_code.l,
+                         FUN = function (v) { v * c(1L, 3L, 1L, 7L, 3L, 9L) })
+  sum_digits.v <- base::vapply(mulw.l, FUN = sum, FUN.VALUE = NA_integer_)
+  check_digit.v <- (10L - sum_digits.v %% 10L) %% 10L
+  as.character(check_digit.v)
+}
+
+sedol_has_correct_checksum <- function(s) {
+  sedol_compute_checksum(substr(s, 1L, 6L)) == substr(s, 7L, 7L)
+}
+
+#' Check validity of SEDOL
+#'
+#' Given a character vector, check the validity of SEDOL
+#' (Stock Exchange Daily Official List)
+#' for each of its elements.
+#'
+#' @param s a character vector for whose elements validity of SEDOL
+#'   is checked.
+#'
+#' @return A logical vector.
+#'
+#' @examples
+#' sedol_check('B014635')
+#' sedol_check('0263494')
+#' sedol_check(c('B014635', '0263494'))
+#' sedol_check(c('B014635', '0263495'))
+#'
+#' @export
+sedol_check <- function(s) {
+  base::nchar(s) == 7L &
+    grepl('^[0-9BCDFGHJKLMNPQRSTVWXYZ]{7}$',
+          s) &
+    sedol_has_correct_checksum(s)
+}
---
+++ tests/testthat/test-sedol_check.R
@@ -0,0 +1,56 @@
+testthat::context("Check SEDOL validity")
+
+testthat::test_that("sedol_check NULL", {
+  testthat::expect_equal(sedol_check(character(0)), logical(0))
+})
+
+test_that("sedol_check correct SEDOL", {
+  testthat::expect_true(sedol_check('5437078'))
+  testthat::expect_true(sedol_check('B923935'))
+  testthat::expect_true(sedol_check('BF8K6K8'))
+  testthat::expect_true(sedol_check('4846288'))
+  testthat::expect_true(sedol_check('0263494'))
+  testthat::expect_true(sedol_check('2515182'))
+  testthat::expect_true(sedol_check('5748521'))
+  testthat::expect_true(sedol_check('2064253'))
+  testthat::expect_true(sedol_check('B28XP76'))
+})
+
+test_that("sedol_check long or short", {
+  # length should be exactly 7 characters
+  testthat::expect_true(!sedol_check('54370780'))
+  testthat::expect_true(!sedol_check('543707'))
+})
+
+test_that("sedol_check vowels", {
+  testthat::expect_true(!sedol_check('A437078'))
+  testthat::expect_true(!sedol_check('E437078'))
+  testthat::expect_true(!sedol_check('I437078'))
+  testthat::expect_true(!sedol_check('U437078'))
+})
+
+test_that("sedol_check no lowercase", {
+  testthat::expect_true(!sedol_check('b923935'))
+})
+
+test_that("sedol_check bad checksum digit", {
+  testthat::expect_true(!sedol_check('5437070'))
+  testthat::expect_true(!sedol_check('5437071'))
+  testthat::expect_true(!sedol_check('5437072'))
+  testthat::expect_true(!sedol_check('5437073'))
+  testthat::expect_true(!sedol_check('5437074'))
+  testthat::expect_true(!sedol_check('5437075'))
+  testthat::expect_true(!sedol_check('5437076'))
+  testthat::expect_true(!sedol_check('5437077'))
+  testthat::expect_true(!sedol_check('5437079'))
+
+  testthat::expect_true(!sedol_check('B923930'))
+  testthat::expect_true(!sedol_check('B923931'))
+  testthat::expect_true(!sedol_check('B923932'))
+  testthat::expect_true(!sedol_check('B923933'))
+  testthat::expect_true(!sedol_check('B923934'))
+  testthat::expect_true(!sedol_check('B923936'))
+  testthat::expect_true(!sedol_check('B923937'))
+  testthat::expect_true(!sedol_check('B923938'))
+  testthat::expect_true(!sedol_check('B923939'))
+})
