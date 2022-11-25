1efbdd1abe30a1e20cea3f22ad24807b1183df7e
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2022-11-25 15:35:18 +0100

    add function names in documentation

--- R/figir.R
+++ R/figir.R
@@ -1,6 +1,7 @@
 #' figir: Check validity of FIGI and other identifiers
 #'
-#' With the functions in this package you can check the
+#' With the functions `figi_check`, `cusip_check`, `isin_check`,
+#' `sedol_check`, in this package, you can check the
 #' validity of the following financial instrument identifiers:
 #' FIGI (Financial Instrument Global Identifier
 #' <https://www.openfigi.com/about/figi>),
@@ -10,6 +11,9 @@
 #' <https://www.cusip.com/identifiers.html#/ISIN>),
 #' SEDOL (Stock Exchange Daily Official List
 #' <https://www2.lseg.com/SEDOL-masterfile-service-tech-guide-v8.6>).
+#' With the function `figi_compute_checksum`,
+#' you can also calculate the FIGI checksum of 11-character strings,
+#' which can be useful if you want to create your own FIGI identifiers.
 #'
 #' @docType package
 #' @name figir
