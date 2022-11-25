47d96bd10c079b768f445cc0e446c75e85be6019
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2022-11-25 15:08:18 +0100

    update description for CRAN

--- R/figir.R
+++ R/figir.R
@@ -1,9 +1,15 @@
-#' figir: Check validity of FIGI
+#' figir: Check validity of FIGI and other identifiers
 #'
-#' This package contains code related to FIGI
-#' (Financial Instrument Global Identifier)
-#' and other financial instrument identifiers
-#' (CUSIP, ISIN, SEDOL).
+#' With the functions in this package you can check the
+#' validity of the following financial instrument identifiers:
+#' FIGI (Financial Instrument Global Identifier
+#' <https://www.openfigi.com/about/figi>),
+#' CUSIP (Committee on Uniform Security Identification Procedures
+#' <https://www.cusip.com/identifiers.html#/CUSIP>),
+#' ISIN (International Securities Identification Number
+#' <https://www.cusip.com/identifiers.html#/ISIN>),
+#' SEDOL (Stock Exchange Daily Official List
+#' <https://www2.lseg.com/SEDOL-masterfile-service-tech-guide-v8.6>).
 #'
 #' @docType package
 #' @name figir
